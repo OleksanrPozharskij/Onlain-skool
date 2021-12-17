8da7280b824e82f68c439b84044257ef19ccd793
OleksanrPozharskij <oleksandrpozarskij@gmail.com>
2021-12-17 20:18:56 +0200

    deploy on heroku

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import django_heroku
 import os
 from django.urls import reverse_lazy
 
@@ -166,3 +166,6 @@ CHANNEL_LAYERS = {
          },
      },
 }
+
+#Herroku settings
+django_heroku.settings(locals())
